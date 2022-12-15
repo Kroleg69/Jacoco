98a27394d195d2efac2c14b989afddd44aeb8d9a
Oleg Sidorov <sidoroffoleg@mail.ru>
2022-12-15 15:16:09 +0300

    New test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,17 +1,28 @@
 package ru.netology.statistic;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StatisticsServiceTest {
 
     @Test
-    public void findMax() {
+    void findMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 50, 3, 8, 6, 11, 11, 12};
+        long expected = 50;
 
         long actual = service.findMax(incomesInBillions);
 
